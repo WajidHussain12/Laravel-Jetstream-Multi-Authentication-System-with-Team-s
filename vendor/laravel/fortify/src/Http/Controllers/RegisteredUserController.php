5b89624006375744c8667d3b550a6a7716fe64e3
WajidHussain <hussainwajid615@gmail.com>
2023-07-12 01:40:33 +0500

    Final With All Files

---
+++ vendor/laravel/fortify/src/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Laravel\Fortify\Http\Controllers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Fortify\Contracts\RegisterResponse;
+use Laravel\Fortify\Contracts\RegisterViewResponse;
+use App\Models\User;
+use App\Models\Role;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * The guard implementation.
+     *
+     * @var \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected $guard;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\StatefulGuard  $guard
+     * @return void
+     */
+    public function __construct(StatefulGuard $guard)
+    {
+        $this->guard = $guard;
+    }
+
+    /**
+     * Show the registration view.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Laravel\Fortify\Contracts\RegisterViewResponse
+     */
+    public function create(Request $request): RegisterViewResponse
+    {
+        return app(RegisterViewResponse::class);
+    }
+
+    /**
+     * Create a new registered user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Laravel\Fortify\Contracts\CreatesNewUsers  $creator
+     * @return \Laravel\Fortify\Contracts\RegisterResponse
+     */
+    public function store(Request $request, CreatesNewUsers $creator)
+    {
+        $user = $creator->create($request->all());
+
+        // Assign default role (ID: 2)
+        $roleId = 2; // Update with the correct default role ID
+        $user->roles()->attach($roleId, ['model_type' => User::class]);
+
+        event(new Registered($user));
+
+        $this->guard->login($user);
+
+        return app(RegisterResponse::class);
+    }
+}
